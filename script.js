459cba63af284f2d4bdc7fcc7cf1c36b4d2e1ba1
vlerOux <vleroux9738@gmail.com>
2024-12-15 12:07:55 +0100

    first commit addon Webscape

---
+++ script.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+const button = document.getElementById('increment-btn');
+const counter = document.getElementById('counter');
+
+button.addEventListener('click', () => {
+  count++;
+//   counter.textContent = count;
+});
